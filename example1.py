2c6e045f796f85a9d583c970ef85b379a9212517
Christina Konstantinidou <103123253+CiciKon@users.noreply.github.com>
2022-05-18 00:14:52 +0300

    Add files via upload

--- example1.py
+++ example1.py
@@ -1,17 +1,21 @@
 import pandas as pd
+import numpy as np
+import matplotlib
 from sklearn import datasets, svm
 from statistics import mean
 from statistics import median
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+
 
 df = pd.read_csv (r'D:\DAI\πτυχιακη\cars\data.csv')
 df.head()
+df.dtypes
 print (df)
 print(df.columns)
 
+#statistics
 for index in range(16):
     if index == 2 or index == 4 or index == 5 or index == 8 or index == 12 or index == 13 or index == 14 or index == 15:
         sum_col = df.iloc[:,index]
@@ -21,53 +25,71 @@ for index in range(16):
         print ("Median ", index, " : % s" % (median(sum_col)))
         print ("Standard Deviation ", index, " : % s" % (np.std(sum_col)))
 
-feature_cols = ['Make', 'Model', 'Year', 'Engine Fuel Type', 'Engine HP',
-               'Engine Cylinders', 'Transmission Type', 'Driven_Wheels',
-               'Number of Doors', 'Vehicle Size', 'Vehicle Style',
-               'highway MPG', 'city mpg', 'Popularity', 'MSRP']
-X = df[feature_cols]
-y = df.MSRP
+#Dropping irrelevant columns
+df = df.drop(['Engine Fuel Type', 'Market Category', 'Vehicle Style', 'Popularity',
+              'Number of Doors', 'Vehicle Size'], axis=1)
+df.head()
+
+df = df.rename(columns={"Engine HP": "HP", "Engine Cylinders": "Cylinders", "Transmission Type":
+                        "Transmission", "Driven_Wheels": "Drive Mode","highway MPG": "MPG-H", "city mpg":
+                        "MPG-C", "MSRP": "Price" })
+df.head()
+
+#Dropping the duplicate rows
+df.shape
+duplicate_rows_df = df[df.duplicated()]
+print("number of dupicate rows:", duplicate_rows_df.shape)
+
+df.count()
+df = df.drop_duplicates()
+df.head(5)
+df.count()
+
+#Dropping the missing or null values
+print(df.isnull().sum())
+
+df = df.dropna()    # Dropping the missing values.
+df.count()
+print(df.isnull().sum())   # After dropping the values
+
+#Prediction Model Building
+X = df[['Year', 'HP', 'Cylinders']]
+y = df.Price
 
 num = int(len(df)*0.8)
 train = df[:num]
 test = df[num:]
 print ("Data:",len(df),", Train:",len(train),", Test:",len(test))
 
-#x_train = np.array(train[['Make', 'Model', 'Year', 'Engine Fuel Type', 'Engine HP',
-#       'Engine Cylinders', 'Transmission Type', 'Driven_Wheels',
-#       'Number of Doors', 'Market Category', 'Vehicle Size', 'Vehicle Style',
-#       'highway MPG', 'city mpg', 'Popularity']])
-#y_train = np.array(train[['MSRP']])
-#x_test = np.array(test[['Make', 'Model', 'Year', 'Engine Fuel Type', 'Engine HP',
-#      'Engine Cylinders', 'Transmission Type', 'Driven_Wheels',
-#       'Number of Doors', 'Market Category', 'Vehicle Size', 'Vehicle Style',
-#      'highway MPG', 'city mpg', 'Popularity']])
-#y_test = np.array(test[['MSRP']])
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+x_train, x_test, y_train, y_test = train_test_split(X.values, y, test_size = 0.2, random_state = 8)
 
-clf = DecisionTreeClassifier()
-clf = clf.fit(X_train,y_train)
-y_pred = clf.predict(X_test)
+# creating an object of LinearRegression class
+lr = LinearRegression()
+# fitting the training data
+lr.fit(x_train,y_train)
+print(lr.score(x_test,y_test))
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print(lr.predict([[2011,300,6]]))
 
-from sklearn.tree import DecisionTreeClassifier
-clf_en = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
-clf_en.fit(X_train, y_train)
+#print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-y_pred_en = clf_en.predict(X_test)
+#from sklearn.tree import DecisionTreeClassifier
+#clf_en = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+#clf_en.fit(X_train, y_train)
+
+#y_pred_en = clf_en.predict(X_test)
+
 
 # Model Accuracy, how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+#print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-from sklearn import tree
-tree.plot_tree(clf_en.fit(X_train, y_train))
+#from sklearn import tree
+#tree.plot_tree(clf_en.fit(X_train, y_train))
 
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred_en)
-print('Confusion matrix\n\n', cm)
+#from sklearn.metrics import confusion_matrix
+#cm = confusion_matrix(y_test, y_pred_en)
+#print('Confusion matrix\n\n', cm)
 
 #regr = linear_model.LinearRegression()
 #regr.fit(x_train,y_train)
